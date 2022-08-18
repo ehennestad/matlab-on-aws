d2617e093469081b9a33ba4801184c9f61752a9c
ehennestad <ehennestad@gmail.com>
2022-08-18 21:43:52 +0200

    Update startup.m

--- startup.m
+++ startup.m
@@ -2,5 +2,13 @@ listing = dir('~/s3-dandi');
 if size(listing,1) < 3
     system('s3fs -o public_bucket=1 -o umask=0022 dandiarchive ~/s3-dandi');
 end
-cd '~/s3-dandi'
+
+listing = dir('~/s3-allen');
+if size(listing,1) < 3
+    system('s3fs -o public_bucket=1 -o umask=0022 allen-brain-observatory ~/s3-allen');
+end
+
 clear all;
+
+addpath(genpath(fullfile('~', 'NANSEN')))
+addpath(genpath(fullfile('~', 'Brain-Observatory-Toolbox')))
