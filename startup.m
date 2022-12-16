29ec38db35a5fd56cb71e6cd3a8b287f30cc2462
ehennestad <ehennestad@gmail.com>
2022-12-16 21:07:59 +0100

    Update startup.m
    Add some startup routines

--- startup.m
+++ startup.m
@@ -12,3 +12,25 @@ clear all;
 
 addpath(genpath(fullfile('~', 'NANSEN')))
 addpath(genpath(fullfile('~', 'Brain-Observatory-Toolbox')))
+
+% Change keyboard to norwegian layout
+system('setxkbmap -layout no');
+
+isInitialized = getpref('MatlabOnAWS', 'IsInitialized', false);
+if ~isInitialized
+    % Change branch of bot repo to dev:
+    system('sudo git config --global --add safe.directory /home/ubuntu/Brain-Observatory-Toolbox');
+    system('sudo git checkout dev');
+    
+    % Need to claim owhnership of folders
+    system('sudo chown -R ubuntu:ubuntu /home/ubuntu/Brain-Observatory-Toolbox')
+    system('sudo chown -R ubuntu:ubuntu /home/ubuntu/NANSEN')
+    system('sudo chown -R ubuntu:ubuntu /home/ubuntu/Documents/MATLAB')
+
+    % Configure bot preferences for cloud 
+    prefs = bot.getPreferences();
+    prefs.UseLocalS3Mount = true;
+    prefs.S3MountDirectory = '/home/ubuntu/s3-allen';
+
+    setpref('MatlabOnAWS', 'IsInitialized', true);
+end
